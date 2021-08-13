c5823ca4e5cfa8e0d402e7e44db92717946d25ba
redclip44 <sparetempextra@gmail.com>
2021-08-13 17:08:26 +0200

    Update ceseek.py

--- ceseek.py
+++ ceseek.py
@@ -34,6 +34,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-#————————————————
-#版权声明：本文为CSDN博主「Wrpzkb」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-#原文链接：https://blog.csdn.net/keyb396/article/details/90138778
+#——————————————.——
+#               CSDN
+#               
